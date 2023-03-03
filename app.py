ceb0ef99f9b5ebfde3df9cbda13caf4011179202
Shivansh1203 <shivansh.rastogi2002@gmail.com>
2023-03-03 16:51:51 +0000

    commit-app1.1-push

--- app.py
+++ app.py
@@ -19,6 +19,91 @@ import smtplib
 
 from shapely.geometry import Point
 
+st.set_page_config(page_title="Team cl_AI_mate", page_icon=":tada:", layout="wide")
+
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+
+
+local_css("style/style.css")
+
+# ---- LOAD ASSETS ----
+lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
+
+
+st.sidebar.header('Team cl_AI_mate')
+
+st.sidebar.subheader('What you want to Predict?')
+time_hist_color = st.sidebar.selectbox('Choose:', ('AQI', 'Heat wave')) 
+
+# st.sidebar.subheader('Choose a city:')
+# donut_theta = st.sidebar.selectbox('Select data', ('Adilabad', 'Nizamabad', 'Karimnagar', 'Khammam', 'Warangal'))
+
+# st.sidebar.subheader('Line chart parameters')
+# plot_data = st.sidebar.multiselect('Select data', ['temperature', 'humidity'], ['temperature', 'humidity'])
+# plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
+
+st.sidebar.markdown('''
+---
+Created with ❤️ by Team cl_AI_mate
+
+''')
+                    
+
+
+with open('Architecture.pdf', "rb") as f:
+    data = f.read()
+b64 = base64.b64encode(data).decode("utf-8")
+pdf_display = f'<embed src="data:application/pdf;base64,{b64}" width="300" height="600" type="application/pdf">'
+st.sidebar.markdown(pdf_display, unsafe_allow_html=True)
+
+
+# ---- HEADER SECTION ----
+with st.container():
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.title("Team cl_AI_mate")
+        st.title("Heatwave Prediction")
+        st.write(
+            "Telangana Tier-2 cities - Alidabad, Nizamabad, Karimnagar, Khammam and Warangal."
+        )
+        st.write("Learn More >")
+
+    with right_column:
+        image = Image.open('images/hw2.jpg')
+        st.image(image)
+       
+
+
+      
+
+
+
+def prepare(df):
+   df['datetime'] = pd.to_datetime(df['datetime'])
+   df.set_index('datetime', inplace=True)
+   df = df.resample('d').max()
+   df = df.reset_index()
+   df['date'] = df['datetime'].dt.date
+   df.set_index('date', inplace=True)
+   T=(df['temp']*9/5)+32  
+   df['temp']=T
+   R=df['humidity']
+   hi = -42.379 + 2.04901523*T + 10.14333127*R - 0.22475541*T*R - 6.83783*(10*-3)(T*T) - 5.481717*(10*-2)*R*R + 1.22874(10*-3)*T*T*R + 8.5282(10*-4)*T*R*R - 1.99(10**-6)*T*T*R*R
+   df['heat_index'] = hi
+   return df
 
 with st.container():
     st.write("---")
